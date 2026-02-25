45e3b42c179de1d7d20ddb681fb566cf005a94fe
ciandre1976 <ciandre23@gmail.com>
2026-02-25 18:51:18 +0100

    add additional authentication routes: verifyEmail, resendVerificationEmail, refreshAccessToken, forgotPassword, resetPassword, and changePassword

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,15 @@ import { userRegisterValidator } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { logoutUser } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getCurrentUser } from "../controllers/auth.controllers.js";
+import {
+  getCurrentUser,
+  verifyEmail,
+  resendVerificationEmail,
+  refreshAccessToken,
+  forgotPassword,
+  resetPassword,
+  changePassword,
+} from "../controllers/auth.controllers.js";
 
 const router = Router();
 
@@ -12,5 +20,13 @@ router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/get-user").get(verifyJWT, getCurrentUser);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router
+  .route("/resend-verification-email")
+  .post(verifyJWT, resendVerificationEmail);
+router.route("/refresh-access-token").post(refreshAccessToken);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:resetToken").post(resetPassword);
+router.route("/change-password").post(verifyJWT, changePassword);
 
 export default router;
