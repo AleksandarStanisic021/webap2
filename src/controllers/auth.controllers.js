216a9e4bce1ae8ac97fa6045bbe74f96992080a3
ciandre1976 <ciandre23@gmail.com>
2026-02-25 17:28:09 +0100

    add user-related functionalities: implement getCurrentUser, verifyEmail, resendVerificationEmail, and refreshAccessToken

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -112,4 +112,109 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
-export { registerUser, loginUser, logoutUser };
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user: req?.user },
+        "Current user fetched successfully",
+      ),
+    );
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { verificationToken } = req.params;
+
+  if (!verificationToken) {
+    throw new ApiError(404, "Invalid or expired verification token");
+  }
+  let hashedToken = crypto
+    .createHash("sha256")
+    .update(verificationToken)
+    .digest("hex");
+  const user = await User.findOne({
+    emailVerificationToken: hashedToken,
+    emailVerificationExpiery: { $gt: Date.now() },
+  });
+  if (!user) {
+    throw new ApiError(404, "Invalid or expired verification token");
+  } else {
+    user.isEmailVerified = true;
+    user.emailVerificationToken = undefined;
+    user.emailVerificationExpiery = undefined;
+    await user.save({ validateBeforeSave: false });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Email verified successfully"));
+  }
+});
+
+const resendVerificationEmail = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  if (user.isEmailVerified) {
+    throw new ApiError(400, "Email is already verified");
+  }
+  const { unhashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiery = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Verify email pls",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unhashedToken}`,
+    ),
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Verification email resent successfully"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const { refreshToken } = req.cookies;
+  if (!refreshToken) {
+    throw new ApiError(404, "Refresh token not found");
+  }
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    throw new ApiError(404, "Invalid refresh token");
+  }
+  const newAccessToken = user.generateAccessToken();
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", newAccessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { accessToken: newAccessToken },
+        "Access token refreshed successfully",
+      ),
+    );
+});
+
+
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+  verifyEmail,
+  resendVerificationEmail,
+  refreshAccessToken,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,11 +4,13 @@ import { userRegisterValidator } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { logoutUser } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getCurrentUser } from "../controllers/auth.controllers.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/get-user").get(verifyJWT, getCurrentUser);
 
 export default router;
