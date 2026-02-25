f86725cbfad42f0da6a6e297c6f50e4925d3d905
ciandre1976 <ciandre23@gmail.com>
2026-02-25 16:20:46 +0100

    add logout functionality and JWT verification middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    throw new ApiError(401, "Unauthorized");
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?.id).select(
+      "-password -emailVerificationToken -refreshToken -emailVerificationExpiry",
+    );
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token");
+  }
+});
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/auth.controllers.js";
 import { userRegisterValidator } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
+import { logoutUser } from "../controllers/auth.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
